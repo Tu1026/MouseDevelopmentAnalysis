5c8e42221e52e789fd640b70c46179cb9a2b503a
Alexander-Morin <alexm.14@outlook.com>
2021-08-20 16:36:54 -0700

    demo of limma using dev stages one vs all contrasts

--- Main/limma_demo.R
+++ Main/limma_demo.R
@@ -15,8 +15,10 @@ library(tximport)
 meta <- read.delim("Data/complete_meta_data.tsv", stringsAsFactors = FALSE)
 pc_mm <- read.delim("Data/ensembl_mouse_protein_coding_104.tsv", stringsAsFactors = FALSE)
 
-# count table files
+# count table files - want to put in same order as meta for dev stages
 files <- list.files("Data/Count_tables", ".tsv", full.names = TRUE)
+file_ids <- str_replace(str_extract(files, "ENCFF.*"), ".tsv", "")
+files <- files[order(match(file_ids, meta$id))]
 names(files) <- meta$id
 
 # use tximport pipeline to load all counts into a matrix
@@ -25,18 +27,31 @@ txi <- tximport(files,
                 type = "rsem",  # ENCODE pipeline uses RSEM for gene counts
                 txIn = FALSE,   # counts are already gene summarized
                 txOut = FALSE,
-                importer = read.delim)  # default read_tsv kept crashing
+                importer = read.delim, # default read_tsv kept crashing
+                countsCol = "expected_count")
 
 # only keep protein coding (must remove gene ID versions) 
 count_mat <- txi$counts
-rownames(count_mat) <- str_replace(rownames(txi$counts), "\\.[:digit:]+$", "")
+rownames(count_mat) <- str_replace(rownames(count_mat), "\\.[:digit:]+$", "")
 count_mat <- count_mat[rownames(count_mat) %in% pc_mm$Gene_ID, ]
 colnames(count_mat) <- paste(meta$id, meta$dev_stage, sep = "_")
+
+# Convert gene ID rownames of count mat to gene symbols
+pc_mm <- pc_mm %>% 
+  filter(Symbol != "") %>% 
+  distinct(Symbol, .keep_all = TRUE)
+
+symbols <- left_join(data.frame(Gene_ID = rownames(count_mat)),
+                     pc_mm[, c("Gene_ID", "Symbol")], by = "Gene_ID")
+
+count_mat <- count_mat[symbols$Gene_ID, ]
+stopifnot(identical(rownames(count_mat), symbols$Gene_ID))
+rownames(count_mat) <- symbols$Symbol
 
 # design matrix for dev stages
 group <- factor(meta$dev_stage)
 design <- model.matrix(~ 0 + group)
-
+colnames(design) <- unique(meta$dev_stage)
 
 # remove genes that are not expressed using edgeR functionality
 dge <- DGEList(count_mat, group = meta$dev_stage)
@@ -56,49 +71,66 @@ cpm_counts <- cpm(dge, log = TRUE, prior.count = 3)
 # fit model with limma eBayes trend 
 fit <- lmFit(cpm_counts, design)
 
-# Define contrast - here, E10.5 versus all. Have to average contributions of 
-# the comparison group
+# Define contrast of each dev stage vs all comparison. 
+# Have to average contributions of the comparison group
 
 contr <- makeContrasts(
-  E10_5 = groupE10.5 - (groupE11.5 + groupE12.5 + groupE13.5 + groupE14.5 + groupE15.5 + groupE16.5 + groupP0)/7,
+  E10.5 = E10.5 - (E11.5 + E12.5 + E13.5 + E14.5 + E15.5 + E16.5 + P0)/(n_distinct(meta$dev_stage)-1),
+  E11.5 = E11.5 - (E10.5 + E12.5 + E13.5 + E14.5 + E15.5 + E16.5 + P0)/(n_distinct(meta$dev_stage)-1),
+  E12.5 = E12.5 - (E10.5 + E11.5 + E13.5 + E14.5 + E15.5 + E16.5 + P0)/(n_distinct(meta$dev_stage)-1),
+  E13.5 = E13.5 - (E10.5 + E11.5 + E12.5 + E14.5 + E15.5 + E16.5 + P0)/(n_distinct(meta$dev_stage)-1),
+  E14.5 = E14.5 - (E10.5 + E11.5 + E12.5 + E13.5 + E15.5 + E16.5 + P0)/(n_distinct(meta$dev_stage)-1),
+  E15.5 = E15.5 - (E10.5 + E11.5 + E12.5 + E13.5 + E14.5 + E16.5 + P0)/(n_distinct(meta$dev_stage)-1),
+  E16.5 = E16.5 - (E10.5 + E11.5 + E12.5 + E13.5 + E14.5 + E15.5 + P0)/(n_distinct(meta$dev_stage)-1),
+  P0 = P0 - (E10.5 + E11.5 + E12.5 + E13.5 + E14.5 + E15.5 + E16.5)/(n_distinct(meta$dev_stage)-1),
   levels = design
 )
 
 
-contr <- makeContrasts(
-  E10_5 = groupE10.5 - (groupE11.5 + groupE12.5 + groupE13.5 + groupE14.5 + groupE15.5 + groupE16.5 + groupP0)/7,
-  levels = design
-)
-
-
-fit <- contrasts.fit(fit, contrasts=contr)
+fit_contr <- contrasts.fit(fit, contrasts=contr)
 fit <- eBayes(fit, trend = TRUE)
+fit_contr <- eBayes(fit_contr, trend = TRUE)
 
+# look at top results for each contrast
 
-# or, explicitly making a one versus all design matrix
+results <- lapply(colnames(design), function(x) {
+  topTable(fit_contr, coef = x, number = Inf)
+})
+names(results) <- colnames(design)
 
-group2 <- factor(meta$dev_stage == 'E10.5')
-design2 <- model.matrix(~ group2)
-fit2 <- eBayes(lmFit(cpm_counts, design2), trend = TRUE)
+topTable(fit_contr, coef = "E10.5", number = Inf)
 
-# look at top results
-de_table <- topTable(fit, number = Inf)
-de_table2 <- topTable(fit2, number = Inf)
+# how many DE genes for each dev stage at FDR05
+lapply(results, function(x) sum(x$adj.P.Val < 0.05))
 
-# include gene symbols and sort
-pc_mm <- distinct(pc_mm, Symbol, .keep_all = TRUE)
+hist(results$E10.5$P.Value, breaks = 100)
 
-de_table_clean <- de_table2 %>% 
-  rownames_to_column(var = "Gene_ID") %>% 
-  left_join(y = pc_mm[, c("Gene_ID", "Symbol")], by = "Gene_ID") %>% 
-  arrange(desc(abs(logFC)))
+# This matrix shows the DE status of each gene for each contrast
+contr_tests <- decideTests(fit_contr)
 
-# how many sig DE genes at FDR05?
-sum(de_table_clean$adj.P.Val < 0.05)
+# example of genes that are never DE
+no_de <- which(apply(contr_tests, 1, function(x) all(x == 0)))
+contr_tests[no_de, ]
 
-# look at most different by logFC
-max_fc <- de_table_clean$Gene_ID[which.max(de_table_clean$logFC)]
-min_fc <- de_table_clean$Gene_ID[which.min(de_table_clean$logFC)]
+plot(cpm_counts["Tbx4", ])
 
-plot(cpm_counts[max_fc, ])
-plot(cpm_counts[min_fc, ])
+# as expected by this design, no genes are DE in every contrast
+all_de <- which(apply(contr_tests, 1, function(x) all(x == 1)))
+contr_tests[all_de, ]
+
+# arbitrary example of gene that is indicative of E10.5-E13.5. consider
+# whether or not the genes must be non-DE/down-reg in the later time points
+ix1 <- which(apply(contr_tests, 1, function(x) all(x[1:4] == 1)))
+ix2 <- which(apply(contr_tests, 1, function(x) all(x[1:4] == 1 & x[5:8] %in% c(0, -1))))
+contr_tests[ix1, ]
+contr_tests[ix2, ]
+plot(cpm_counts["Cdc45", ])
+
+setdiff(ix1, ix2)
+contr_tests[setdiff(ix1, ix2)[1], ]
+plot(cpm_counts["Zfp276",])
+
+# this ranks the most variable genes over all contrasts
+topTable(fit_contr, number = 30)
+
+plot(cpm_counts["Igf2bp1", ])
